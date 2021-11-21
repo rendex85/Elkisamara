058f13c20bb975574def47d2279356bfbeb709e9
Lev <tikhonov.lev0@gmail.com>
2021-11-21 11:14:22 +0300

    up

--- Elkisamara/urls.py
+++ Elkisamara/urls.py
@@ -1,21 +1,28 @@
-"""Elkisamara URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 
+from .views import (
+    BaseView,
+    ProductDetailView,
+    CategoryDetailView,
+    CartView,
+    AddToCartView,
+    DeleteFromCartView,
+    ChangeQTYView,
+    CheckoutView,
+    MakeOrderView
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', BaseView.as_view(), name='base'),
+    path('products/<str:ct_model>/<str:slug>/', ProductDetailView.as_view(), name='product_detail'),
+    path('category/<str:slug>/', CategoryDetailView.as_view(), name='category_detail'),
+    path('cart/', CartView.as_view(), name='cart'),
+    path('add-to-cart/<str:ct_model>/<str:slug>/', AddToCartView.as_view(), name='add_to_cart'),
+    path('remove-from-cart/<str:ct_model>/<str:slug>/', DeleteFromCartView.as_view(), name='delete_from_cart'),
+    path('change-qty/<str:ct_model>/<str:slug>/', ChangeQTYView.as_view(), name='change_qty'),
+    path('checkout/', CheckoutView.as_view(), name='checkout'),
+    path('make-order/', MakeOrderView.as_view(), name='make_order')
+
 ]
