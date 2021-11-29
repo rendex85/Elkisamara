3c38a1e7a7fc4eb1e2ebe2a91b5e82936b17bd37
Lev <tikhonov.lev0@gmail.com>
2021-11-29 23:13:38 +0300

    edit models by pep

--- main/models.py
+++ main/models.py
@@ -15,25 +15,30 @@ def get_models_for_count(*model_names):
 
 def get_product_url(obj, viewname):
     ct_model = obj.__class__._meta.model_name
-    return reverse(viewname, kwargs={'ct_model': ct_model, 'slug': obj.slug})
+    return reverse(viewname, kwargs={"ct_model": ct_model, "slug": obj.slug})
 
 
 class LatestProductsManager:
-
     @staticmethod
     def get_products_for_main_page(*args, **kwargs):
-        with_respect_to = kwargs.get('with_respect_to')
+        with_respect_to = kwargs.get("with_respect_to")
         products = []
         ct_models = ContentType.objects.filter(model__in=args)
         for ct_model in ct_models:
-            model_products = ct_model.model_class()._base_manager.all().order_by('-id')[:5]
+            model_products = (
+                ct_model.model_class()._base_manager.all().order_by("-id")[:5]
+            )
             products.extend(model_products)
         if with_respect_to:
             ct_model = ContentType.objects.filter(model=with_respect_to)
             if ct_model.exists():
                 if with_respect_to in args:
                     return sorted(
-                        products, key=lambda x: x.__class__._meta.model_name.startswith(with_respect_to), reverse=True
+                        products,
+                        key=lambda x: x.__class__._meta.model_name.startswith(
+                            with_respect_to
+                        ),
+                        reverse=True,
                     )
         return products
 
@@ -43,26 +48,27 @@ class LatestProducts:
 
 
 class CategoryManager(models.Manager):
-    CATEGORY_NAME_COUNT_NAME = {
-        'Елки': 'tree__count',
-        'Смартфоны': 'smartphone__count'
-    }
+    CATEGORY_NAME_COUNT_NAME = {"Елки": "tree__count", "Смартфоны": "smartphone__count"}
 
     def get_queryset(self):
         return super().get_queryset()
 
     def get_categories_for_left_sidebar(self):
-        models = get_models_for_count('notebook', 'smartphone')
+        models = get_models_for_count("notebook", "smartphone")
         qs = list(self.get_queryset().annotate(*models))
         data = [
-            dict(name=c.name, url=c.get_absolute_url(), count=getattr(c, self.CATEGORY_NAME_COUNT_NAME[c.name]))
+            dict(
+                name=c.name,
+                url=c.get_absolute_url(),
+                count=getattr(c, self.CATEGORY_NAME_COUNT_NAME[c.name]),
+            )
             for c in qs
         ]
         return data
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=255, verbose_name='Имя категории')
+    name = models.CharField(max_length=255, verbose_name="Имя категории")
     slug = models.SlugField(unique=True)
     objects = CategoryManager()
 
@@ -70,24 +76,34 @@ class Category(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('category_detail', kwargs={'slug': self.slug})
+        return reverse("category_detail", kwargs={"slug": self.slug})
 
     class Meta:
-        verbose_name = 'Категория товара'
-        verbose_name_plural = 'Категории товаров'
+        verbose_name = "Категория товара"
+        verbose_name_plural = "Категории товаров"
 
 
 class Product(models.Model):
     class Meta:
         abstract = True
 
-    category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE)
-    title = models.CharField(max_length=255, verbose_name='Наименование')
+    category = models.ForeignKey(
+        Category, verbose_name="Категория", on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=255, verbose_name="Наименование")
     slug = models.SlugField(unique=True)
     # Надо заменить дефолт=нулл на плейсхолдер для товаров без изображения
-    image = models.ImageField(verbose_name='Изображение', upload_to="products", null=True, blank=True, default=None)
-    description = models.TextField(verbose_name='Описание', null=True, blank=True)
-    price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Цена, руб')
+    image = models.ImageField(
+        verbose_name="Изображение",
+        upload_to="products",
+        null=True,
+        blank=True,
+        default=None,
+    )
+    description = models.TextField(verbose_name="Описание", null=True, blank=True)
+    price = models.DecimalField(
+        max_digits=9, decimal_places=2, verbose_name="Цена, руб"
+    )
 
     def image_tag(self):
         resize_img = 1
@@ -96,9 +112,10 @@ class Product(models.Model):
         else:
             resize_img = self.image.width / 400
         return mark_safe(
-            f'<img src={self.image.url} width="{self.image.width / resize_img}" height="{self.image.height / resize_img}" />')
+            f'<img src={self.image.url} width="{self.image.width / resize_img}" height="{self.image.height / resize_img}" />'
+        )
 
-    image_tag.short_description = 'Изображение товара'
+    image_tag.short_description = "Изображение товара"
 
     def __str__(self):
         return self.title
@@ -107,25 +124,34 @@ class Product(models.Model):
         return self.__class__.__name__.lower()
 
 
-class Notebook(Product):
-
 class ChristmasTree(Product):
-    height = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Рост елки, м', null=True, blank=True)
-    tree_type = models.CharField(max_length=255, verbose_name='Тип дерева', null=True, blank=True)
-    weight = models.IntegerField(verbose_name='Вес, кг', null=True, blank=True)
-    from_place = models.CharField(max_length=512, verbose_name='Откуда привезена', null=True, blank=True)
-    image = models.ImageField(verbose_name='Изображение', upload_to="products/tree", null=True, blank=True)
+    height = models.DecimalField(
+        max_digits=9,
+        decimal_places=2,
+        verbose_name="Рост елки, м",
+        null=True,
+        blank=True,
+    )
+    tree_type = models.CharField(
+        max_length=255, verbose_name="Тип дерева", null=True, blank=True
+    )
+    weight = models.IntegerField(verbose_name="Вес, кг", null=True, blank=True)
+    from_place = models.CharField(
+        max_length=512, verbose_name="Откуда привезена", null=True, blank=True
+    )
+    image = models.ImageField(
+        verbose_name="Изображение", upload_to="products/tree", null=True, blank=True
+    )
 
     def __str__(self):
         return "{} : {}".format(self.category.name, self.title)
 
     def get_absolute_url(self):
-        return get_product_url(self, 'product_detail')
+        return get_product_url(self, "product_detail")
 
-      
     class Meta:
-        verbose_name = 'Товар: Елка'
-        verbose_name_plural = 'Товар: Елки'
+        verbose_name = "Товар: Елка"
+        verbose_name_plural = "Товар: Елки"
 
 
 """
@@ -151,19 +177,26 @@ class Smartphone(Product):
 
 """
 
-class CartProduct(models.Model):
-
-
 
 class CartProduct(models.Model):
-    user = models.ForeignKey('Customer', verbose_name='Покупатель', on_delete=models.CASCADE)
-    cart = models.ForeignKey('Cart', verbose_name='Корзина', on_delete=models.CASCADE, related_name='related_products',
-                             null=True, blank=True)
+    user = models.ForeignKey(
+        "Customer", verbose_name="Покупатель", on_delete=models.CASCADE
+    )
+    cart = models.ForeignKey(
+        "Cart",
+        verbose_name="Корзина",
+        on_delete=models.CASCADE,
+        related_name="related_products",
+        null=True,
+        blank=True,
+    )
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
-    content_object = GenericForeignKey('content_type', 'object_id')
+    content_object = GenericForeignKey("content_type", "object_id")
     qty = models.PositiveIntegerField(default=1)
-    final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Общая цена', null=True, blank=True)
+    final_price = models.DecimalField(
+        max_digits=9, decimal_places=2, verbose_name="Общая цена", null=True, blank=True
+    )
 
     def __str__(self):
         return f"{self.content_object.category}: {self.content_object.title} (id: {self.object_id}, кол-во: {self.qty})"
@@ -173,89 +206,132 @@ class CartProduct(models.Model):
         super().save(*args, **kwargs)
 
     class Meta:
-        verbose_name = 'Продукт в корзине'
-        verbose_name_plural = 'Продукты в корзине'
+        verbose_name = "Продукт в корзине"
+        verbose_name_plural = "Продукты в корзине"
 
 
 class Cart(models.Model):
-    owner = models.ForeignKey('Customer', null=True, verbose_name='Владелец', on_delete=models.CASCADE)
-    products = models.ManyToManyField(CartProduct, blank=True, related_name='related_cart',
-                                      verbose_name='Товары в корзине')
-    total_products = models.PositiveIntegerField(default=0, verbose_name="Всего товара, шт")
-    final_price = models.DecimalField(max_digits=9, default=0, decimal_places=2, verbose_name='Общая цена, руб')
+    owner = models.ForeignKey(
+        "Customer", null=True, verbose_name="Владелец", on_delete=models.CASCADE
+    )
+    products = models.ManyToManyField(
+        CartProduct,
+        blank=True,
+        related_name="related_cart",
+        verbose_name="Товары в корзине",
+    )
+    total_products = models.PositiveIntegerField(
+        default=0, verbose_name="Всего товара, шт"
+    )
+    final_price = models.DecimalField(
+        max_digits=9, default=0, decimal_places=2, verbose_name="Общая цена, руб"
+    )
     in_order = models.BooleanField(default=False, verbose_name="Оформлен ли заказ?")
-    for_anonymous_user = models.BooleanField(default=False, verbose_name="Зарегистрован ли пользователь?")
+    for_anonymous_user = models.BooleanField(
+        default=False, verbose_name="Зарегистрован ли пользователь?"
+    )
 
     def __str__(self):
         return str(self.id)
 
     class Meta:
-        verbose_name = 'Корзина  пользователя'
-        verbose_name_plural = 'Корзины пользователя'
+        verbose_name = "Корзина  пользователя"
+        verbose_name_plural = "Корзины пользователя"
 
 
 class Customer(models.Model):
-    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
-    phone = models.CharField(max_length=20, verbose_name='Номер телефона')
-    address = models.CharField(max_length=255, verbose_name='Адрес', null=True, blank=True)
-    orders = models.ManyToManyField('Order', verbose_name='Заказы покупателя', related_name='related_order', null=True,
-                                    blank=True)
+    user = models.ForeignKey(
+        User, verbose_name="Пользователь", on_delete=models.CASCADE
+    )
+    phone = models.CharField(max_length=20, verbose_name="Номер телефона")
+    address = models.CharField(
+        max_length=255, verbose_name="Адрес", null=True, blank=True
+    )
+    orders = models.ManyToManyField(
+        "Order",
+        verbose_name="Заказы покупателя",
+        related_name="related_order",
+        null=True,
+        blank=True,
+    )
 
     def __str__(self):
         return "Покупатель: {} {}".format(self.user.first_name, self.user.last_name)
 
     class Meta:
-        verbose_name = 'Покупатель'
-        verbose_name_plural = 'Покупатели'
+        verbose_name = "Покупатель"
+        verbose_name_plural = "Покупатели"
 
 
 class Order(models.Model):
-    STATUS_NEW = 'new'
-    STATUS_IN_PROGRESS = 'in_progress'
-    STATUS_READY = 'is_ready'
-    STATUS_COMPLETED = 'completed'
+    STATUS_NEW = "new"
+    STATUS_IN_PROGRESS = "in_progress"
+    STATUS_READY = "is_ready"
+    STATUS_COMPLETED = "completed"
 
-    BUYING_TYPE_SELF = 'self'
-    BUYING_TYPE_DELIVERY = 'delivery'
+    BUYING_TYPE_SELF = "self"
+    BUYING_TYPE_DELIVERY = "delivery"
 
     STATUS_CHOICES = (
-        (STATUS_NEW, 'Новый заказ'),
-        (STATUS_IN_PROGRESS, 'Заказ в обработке'),
-        (STATUS_READY, 'Заказ готов'),
-        (STATUS_COMPLETED, 'Заказ выполнен')
+        (STATUS_NEW, "Новый заказ"),
+        (STATUS_IN_PROGRESS, "Заказ в обработке"),
+        (STATUS_READY, "Заказ готов"),
+        (STATUS_COMPLETED, "Заказ выполнен"),
     )
 
     BUYING_TYPE_CHOICES = (
-        (BUYING_TYPE_SELF, 'Самовывоз'),
-        (BUYING_TYPE_DELIVERY, 'Доставка')
+        (BUYING_TYPE_SELF, "Самовывоз"),
+        (BUYING_TYPE_DELIVERY, "Доставка"),
     )
 
-    customer = models.ForeignKey(Customer, verbose_name='Покупатель', related_name='related_orders',
-                                 on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=255, verbose_name='Имя', null=True, blank=True)
-    last_name = models.CharField(max_length=255, verbose_name='Фамилия', null=True, blank=True)
-    phone = models.CharField(max_length=20, verbose_name='Телефон', null=True, blank=True, default=None)
-    cart = models.ForeignKey(Cart, verbose_name='Корзина', on_delete=models.CASCADE)
-    address = models.CharField(max_length=1024, verbose_name='Адрес', null=True, blank=True)
+    customer = models.ForeignKey(
+        Customer,
+        verbose_name="Покупатель",
+        related_name="related_orders",
+        on_delete=models.CASCADE,
+    )
+    first_name = models.CharField(
+        max_length=255, verbose_name="Имя", null=True, blank=True
+    )
+    last_name = models.CharField(
+        max_length=255, verbose_name="Фамилия", null=True, blank=True
+    )
+    phone = models.CharField(
+        max_length=20, verbose_name="Телефон", null=True, blank=True, default=None
+    )
+    cart = models.ForeignKey(Cart, verbose_name="Корзина", on_delete=models.CASCADE)
+    address = models.CharField(
+        max_length=1024, verbose_name="Адрес", null=True, blank=True
+    )
     status = models.CharField(
         max_length=100,
-        verbose_name='Статус заказ',
+        verbose_name="Статус заказ",
         choices=STATUS_CHOICES,
-        default=STATUS_NEW
+        default=STATUS_NEW,
     )
     buying_type = models.CharField(
         max_length=100,
-        verbose_name='Тип заказа',
+        verbose_name="Тип заказа",
         choices=BUYING_TYPE_CHOICES,
-        default=BUYING_TYPE_SELF
+        default=BUYING_TYPE_SELF,
     )
-    comment = models.TextField(verbose_name='Комментарий к заказу', null=True, blank=True)
-    created_at = models.DateTimeField(auto_now=True, verbose_name='Дата создания заказа')
-    order_date = models.DateField(verbose_name='Дата получения заказа', default=timezone.now)
-    order_content_description = models.TextField(verbose_name='Описания составляющих заказа', null=True, blank=True)
+    comment = models.TextField(
+        verbose_name="Комментарий к заказу", null=True, blank=True
+    )
+    created_at = models.DateTimeField(
+        auto_now=True, verbose_name="Дата создания заказа"
+    )
+    order_date = models.DateField(
+        verbose_name="Дата получения заказа", default=timezone.now
+    )
+    order_content_description = models.TextField(
+        verbose_name="Описания составляющих заказа", null=True, blank=True
+    )
 
     def __str__(self):
-        return f"Заказ номер: {self.id}, дата: {self.created_at.date()},  {self.customer} "
+        return (
+            f"Заказ номер: {self.id}, дата: {self.created_at.date()},  {self.customer} "
+        )
 
     def save(self, *args, **kwargs):
         # Если имя, фамилия и телефон в заказе пустые - указываем данные самого пользователя
@@ -268,16 +344,20 @@ class Order(models.Model):
             self.address = self.customer.address
 
         # Составляем описание заказа
-        description_string = f"Заказ №{self.id}\nОбщая сумма заказа: {self.cart.final_price} руб., " \
-                             f"общее число товаров: {self.cart.total_products} шт.\n\n" \
-                             f"Товары:\n"
+        description_string = (
+            f"Заказ №{self.id}\nОбщая сумма заказа: {self.cart.final_price} руб., "
+            f"общее число товаров: {self.cart.total_products} шт.\n\n"
+            f"Товары:\n"
+        )
 
         for product in self.cart.products.all():
-            description_string += f"Наименование: {product.content_object.title}. Количество: {product.qty} шт., " \
-                                  f"суммарная стоимость: {product.final_price} руб.\n"
+            description_string += (
+                f"Наименование: {product.content_object.title}. Количество: {product.qty} шт., "
+                f"суммарная стоимость: {product.final_price} руб.\n"
+            )
         self.order_content_description = description_string
         super().save(*args, **kwargs)
 
     class Meta:
-        verbose_name = 'Заказ'
-        verbose_name_plural = 'Заказы'
+        verbose_name = "Заказ"
+        verbose_name_plural = "Заказы"
